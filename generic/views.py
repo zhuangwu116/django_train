20ef099c2c9f1ae206bad7588cca7fa98bb60fae
zhuangwu <zhuangwu@zkhc.org>
2017-07-07 15:39:21 +0800

    ?

--- generic/views.py
+++ generic/views.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 class LoginRequireMixin(object):
@@ -9,5 +8,23 @@ class LoginRequireMixin(object):
     def as_view(cls,**initkwargs):
         view=super(LoginRequireMixin,cls).as_view(**initkwargs)
         return login_required(view)
-class MyView(LoginRequireMixin):
-    pass
+
+
+from django.http import JsonResponse
+
+class AjaxableResponseMixin(object):
+    def form_invalid(self,form):
+        response=super(AjaxableResponseMixin,self).form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse(form.errors,status=400)
+        else:
+            return response
+    def form_valid(self,form):
+        response = super(AjaxableResponseMixin,self).form_invalid(form)
+        if self.request.is_ajax():
+            data={
+                'pk':self.object.pk,
+            }
+            return JsonResponse(data)
+        else:
+            return response
