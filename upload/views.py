847d0e16dbb652b479d1113a0018f24cb5f07818
zhuangwu <zhuangwu@zkhc.org>
2017-07-14 15:03:41 +0800

    ?

--- upload/views.py
+++ upload/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render,reverse
 from django.http import HttpResponseRedirect
 from django.views.generic.edit import FormView
-from .forms import UploadFileForm
+from .forms import UploadFileForm,FileFieldForm
 
 # Create your views here.
 def handle_uploaded_file(f):
@@ -33,13 +33,15 @@ class UploadFormView(FormView):
 #
 # If you want to upload multiple files using one form field, set the multiple HTML attribute of field’s widget:
 class FileFieldView(FormView):
-    form_class = UploadFileForm
+    form_class = FileFieldForm
     template_name = 'upload/upload.html'
     success_url = '/base_view/thanks/'
     def post(self, request, *args, **kwargs):
         form_class = self.get_form_class()
         form = self.get_form(form_class)
         files = request.FILES.getlist('filed_field')
+        if files:
+            print('aaaaaaaaaaa')
         if form.is_valid():
             for f in files:
                 pass
