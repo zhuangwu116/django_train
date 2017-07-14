b57adff17669fb416ac7996865e3e58cb1193336
zhuangwu <zhuangwu@zkhc.org>
2017-07-14 14:55:54 +0800

    ?

--- upload/urls.py
+++ upload/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 import views
 urlpatterns=[
     url(r'^',views.UploadFormView.as_view(),name='upload_index'),
-    url(r'^', views.FileFieldView.as_view(), name='filefield'),
+    url(r'^filefield', views.FileFieldView.as_view(), name='filefield'),
 ]
