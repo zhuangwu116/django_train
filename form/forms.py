4d2c1ad81c78e47a34c39b0355f6c61ba276b9ac
zhuangwu <zhuangwu@zkhc.org>
2017-07-31 15:11:43 +0800

    ?

--- form/forms.py
+++ form/forms.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django import forms
 from django.forms import formset_factory
+from django.forms.formsets import BaseFormSet
+
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
@@ -13,4 +15,20 @@ class ArticleForm(forms.Form):
     title = forms.CharField()
     pub_date = forms.DateField()
 
-ArticleFormSet = formset_factory(ArticleForm,extra=2)
+#表单集是同一个页面上多个表单的抽象。它非常类似于一个数据表格。假设有下述表单：
+#正如你所看到的，这里仅显示一个空表单。显示的表单的数目通过extra 参数控制。默认情况下，formset_factory() 定义一个表单；下面的示例将显示两个空表单：
+ArticleFormSet = formset_factory(ArticleForm)
+ArticleFormSetS = formset_factory(ArticleForm,extra=2)
+
+#自定义表单集验证
+class BaseArticleFormSet(BaseFormSet):
+    def clean(self):
+        if any(self.errors):
+            return
+        titles = []
+        for form in self.forms:
+            title = form.cleaned_data['title']
+            if title in titles:
+                raise forms.ValidationError("Articles in a set must have distinct titles.")
+            titles.append(title)
+CustomArticleFormSet = formset_factory(ArticleFormSet,formset=BaseArticleFormSet)
--- form/views.py
+++ form/views.py
@@ -27,3 +27,57 @@ def articleformset(request):
         'pub_date': datetime.date.today(),
     }])
     return render(request,'form/articleformset.html',{"formset":formset})
+
+def formset_validation(request):
+    data = {
+        'form-TOTAL_FORMS': '1',
+        'form-INITIAL_FORMS': '0',
+        'form-MAX_NUM_FORMS': '',
+    }
+    formset = ArticleFormSet(data)
+    print formset.is_valid()
+
+    data.update({
+        'form-0-title': 'Test',
+        'form-0-pub_date': '1904-16-16',
+        'form-1-title': 'Test',
+        'form-1-pub_date': '',
+    })
+    formset = ArticleFormSet(data)
+    print formset.is_valid()
+    print formset.errors
+    print len(formset.errors)
+    #想知道表单集内有多少个错误可以使用total_error_count方法
+    print formset.total_error_count()
+    # ManagementForm¶
+    # 你也许已经注意到了那些附加的数据 (form-TOTAL_FORMS, form-INITIAL_FORMS and form-MAX_NUM_FORMS)
+    # 他们是必要的，且必须位于表单集数据的最上方 这些必须传递给ManagementForm. ManagementFormThis
+    # 用于管理表单集中的表单. 如果你不提供这些数据，将会触发异常
+    data = {
+        'form-TOTAL_FORMS': '1',
+        'form-INITIAL_FORMS': '0',
+        'form-MAX_NUM_FORMS': '',
+        'form-0-title': '',
+        'form-0-pub_date': '',
+    }
+    formset = ArticleFormSet(data)
+    #我们也可以检查表单数据是否从初:始值发生了变化 (i.e. the form was sent without any data
+    print formset.has_changed()
+    #total_form_count返回此表单集中的表单总数
+    return render(request,'form/articleformset.html',{'formset':formset})
+
+def customformset_valid(request):
+    data = {
+        'form-TOTAL_FORMS': '2',
+        'form-INITIAL_FORMS': '0',
+        'form-MAX_NUM_FORMS': '',
+        'form-0-title': 'Test',
+        'form-0-pub_date': '1904-06-16',
+        'form-1-title': 'Test',
+        'form-1-pub_date': '1912-06-23',
+    }
+    formset = ArticleFormSet(data)
+    print formset.is_valid()
+    print formset.errors
+    print formset.non_form_errors()
+    return render(request, 'form/articleformset.html', {'formset': formset})
